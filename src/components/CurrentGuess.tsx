0f1c94f48d5b36212590a70104b77ee67df91c0b
Ido Kadosh <1234kadosh@gmail.com>
2024-07-16 16:32:38 +0300

    style: add animation for successful guess

--- src/components/CurrentGuess.tsx
+++ src/components/CurrentGuess.tsx
@@ -1,7 +1,54 @@
+import { useEffect, useState } from 'react';
+
 interface PropTypes {
 	guess: string;
+	shouldHighlight: boolean;
+	onHighlightComplete: () => void;
 }
-const CurrentGuess = ({ guess }: PropTypes) => {
+const CurrentGuess = ({ guess, shouldHighlight, onHighlightComplete }: PropTypes) => {
+	const [highlightIndex, setHighlightIndex] = useState<number>(-1);
+	const [slideIndex, setSlideIndex] = useState<number>(-1);
+
+	useEffect(() => {
+		// highlight start
+		if (shouldHighlight && highlightIndex < 5) {
+			const timer = setTimeout(() => {
+				setHighlightIndex(prev => prev + 1);
+			}, 100);
+			return () => clearTimeout(timer);
+		}
+
+		// slide start
+		if (highlightIndex === 5 && shouldHighlight) {
+			setTimeout(() => {
+				setSlideIndex(0);
+			}, 250);
+			return;
+		}
+
+		// reset slide and highlight indices
+		if (!shouldHighlight) {
+			setHighlightIndex(-1);
+			setSlideIndex(-1);
+		}
+	}, [highlightIndex, shouldHighlight]);
+
+	useEffect(() => {
+		//slide start
+		if (slideIndex >= 0 && slideIndex < 5) {
+			const timer = setTimeout(() => {
+				setSlideIndex(prev => prev + 1);
+			}, 100);
+			return () => clearTimeout(timer);
+		}
+
+		if (slideIndex === 5) {
+			setTimeout(() => {
+				onHighlightComplete();
+			}, 750);
+		}
+	}, [slideIndex]);
+
 	const getBoxesToDisplay = () => {
 		let boxes = [];
 		for (let i = 0; i < 5; i++) {
@@ -10,20 +57,29 @@ const CurrentGuess = ({ guess }: PropTypes) => {
 		return boxes;
 	};
 
+	const getBoxClassName = (letter: string, idx: number) => {
+		let className = '';
+		if (idx < slideIndex) className += 'animate-slideUp ';
+		if (idx <= highlightIndex) className += 'bg-blue-200';
+		else if (letter) className += 'bg-orange';
+		return className;
+	};
+
 	return (
-		<div>
-			<div className="flex gap-1">
-				{getBoxesToDisplay().map((letter, idx) => (
+		<div className="flex gap-1">
+			{getBoxesToDisplay().map((letter, idx) => (
+				<div className="relative mb-3" key={idx}>
+					<div className="absolute inset-0 border-2 border-slate-500"></div>
 					<div
-						className={` ${
-							letter ? 'bg-orange' : 'bg-white border-2 border-slate-500'
-						} mb-3 text-4xl font-semibold w-20 grid place-items-center aspect-square text-white`}
-						key={idx}
+						className={`${getBoxClassName(
+							letter,
+							idx
+						)} text-4xl font-semibold w-20 grid place-items-center aspect-square text-white relative`}
 					>
 						{letter}
 					</div>
-				))}
-			</div>
+				</div>
+			))}
 		</div>
 	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,24 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				blue: '#03a9f5',
+				blue: {
+					100: '#03a9f5',
+					200: '#0277BD',
+				},
 				orange: '#f67b01',
 				gray: {
 					100: '#333333',
 					200: '#cccccc',
 				},
+			},
+			keyframes: {
+				slideUp: {
+					'0%': { transform: 'translateY(0)' },
+					'100%': { transform: 'translateY(-115%)' },
+				},
+			},
+			animation: {
+				slideUp: 'slideUp 0.5s forwards',
 			},
 		},
 	},
