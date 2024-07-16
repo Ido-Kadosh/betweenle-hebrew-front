52ced53bb691ba3c99f3196d0dd136094ccc78b6
Ido Kadosh <1234kadosh@gmail.com>
2024-07-16 17:43:12 +0300

    feat: add onGuess function to start sliding animation

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -3,23 +3,16 @@ import CurrentGuess from '../components/CurrentGuess';
 import PreviousGuess from '../components/PreviousGuess';
 import { useLocalStorage } from '@uidotdev/usehooks';
 import Keyboard from '../components/Keyboard';
+import { gameService } from '../services/game.service';
+import { showErrorMsg } from '../services/eventBus.service';
+import RemainingLetters from '../components/RemainingLetters';
+import { GuessDirection } from '../types/Game';
 
 const GamePage = () => {
 	const [topGuess, setTopGuess] = useLocalStorage('topGuess', 'אאאאא');
 	const [bottomGuess, setBottomGuess] = useLocalStorage('bottomGuess', 'תתתתת');
 	const [guess, setGuess] = useState<string>('');
-
-	const handleKeyPress = useCallback(
-		(key: string) => {
-			if (guess.length < 5 && /^[א-ת]$/.test(key)) {
-				setGuess(prevGuess => prevGuess + key);
-			}
-			if (guess.length > 0 && (key === 'Backspace' || key === 'Delete')) {
-				setGuess(prevGuess => prevGuess.slice(0, -1));
-			}
-		},
-		[guess]
-	);
+	const [slideDirection, setSlideDirection] = useState<GuessDirection | null>(null);
 
 	useEffect(() => {
 		const onKeyPress = (ev: KeyboardEvent) => {
@@ -33,6 +26,41 @@ const GamePage = () => {
 		};
 	}, [guess]);
 
+	const onHighlightComplete = useCallback(
+		(direction: GuessDirection | null) => {
+			const method = direction === 'TOP' ? setTopGuess : setBottomGuess;
+			// make sure to set previous guess before setting guess to empty string
+			setGuess(() => {
+				method(guess);
+				return '';
+			});
+			setSlideDirection(null);
+		},
+		[guess]
+	);
+
+	const handleKeyPress = useCallback(
+		(key: string) => {
+			if (guess.length > 0 && (key === 'Backspace' || key === 'Delete')) {
+				setGuess(prevGuess => prevGuess.slice(0, -1));
+			} else if (guess.length < 5 && /^[א-ת]$/.test(key)) {
+				setGuess(prevGuess => prevGuess + key);
+			} else if (key === 'Enter') {
+				onGuess();
+			}
+		},
+		[guess]
+	);
+
+	const onGuess = async () => {
+		try {
+			const res = await gameService.guessWord(guess);
+			setSlideDirection(res);
+		} catch (err) {
+			showErrorMsg('אין מספיק אותיות!');
+		}
+	};
+
 	return (
 		<div className="flex flex-col">
 			<header className="mt-2">
@@ -40,10 +68,11 @@ const GamePage = () => {
 			</header>
 			<div className="self-center mt-5 flex flex-col items-center">
 				<div>
-					<PreviousGuess word={topGuess} />
-					<CurrentGuess guess={guess} />
-					<PreviousGuess word={bottomGuess} />
+					<PreviousGuess guess={topGuess} />
+					<CurrentGuess guess={guess} slideDirection={slideDirection} onHighlightComplete={onHighlightComplete} />
+					<PreviousGuess guess={bottomGuess} />
 				</div>
+				<RemainingLetters />
 				<Keyboard onKeyPress={handleKeyPress} />
 			</div>
 		</div>
