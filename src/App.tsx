4581756d4459d9f947b88e059f1c8fb9b2695837
Ido Kadosh <1234kadosh@gmail.com>
2024-07-21 12:35:25 +0300

    feat: add routing for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import MsgProvider from './contexts/MsgContext/MsgProvider';
 import GamePage from './pages/GamePage';
+import TutorialPage from './pages/TutorialPage';
+import HomePage from './pages/HomePage';
 
 function App() {
 	return (
-		<MsgProvider>
-			<main className="max-w-4xl m-auto h-screen">
-				<GamePage />
-			</main>
-		</MsgProvider>
+		<Router>
+			<MsgProvider>
+				<main className="max-w-4xl m-auto h-screen px-4 pt-4">
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/daily" element={<GamePage />} />
+						<Route path="/tutorial" element={<TutorialPage />} />
+					</Routes>
+				</main>
+			</MsgProvider>
+		</Router>
 	);
 }
 
