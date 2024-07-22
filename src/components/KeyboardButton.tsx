be4b1812277711bf44e99dc56956667551510c91
Ido Kadosh <1234kadosh@gmail.com>
2024-07-22 13:50:12 +0300

    fix: remove focus on KeyboardButton component when keyboard is pressed

--- src/components/KeyboardButton.tsx
+++ src/components/KeyboardButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useMsg } from '../contexts/MsgContext/useMsg';
 
 interface PropTypes {
@@ -8,15 +8,39 @@ interface PropTypes {
 
 const KeyboardButton = ({ children, onClick }: PropTypes) => {
 	const { setMsg } = useMsg();
+	const buttonRef = useRef<HTMLButtonElement>(null);
+
+	// backspace makes buttons focused, and disables 'Enter' to send
+	// since pressing keyboard also works, we don't need focus for accessibility.
+	useEffect(() => {
+		const handleFocus = () => {
+			if (buttonRef.current) {
+				buttonRef.current.blur();
+			}
+		};
+
+		if (buttonRef.current) {
+			buttonRef.current.addEventListener('focus', handleFocus);
+		}
+
+		return () => {
+			if (buttonRef.current) {
+				buttonRef.current.removeEventListener('focus', handleFocus);
+			}
+		};
+	}, []);
+
 	const handleClick = (ev: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-		setMsg(null);
+		setMsg(null); // hide message for better user experience
 		onClick(ev);
 	};
 
 	return (
 		<button
+			ref={buttonRef}
+			tabIndex={-1}
 			onClick={handleClick}
-			className="text-gray-100 bg-gray-200 rounded-lg font-semibold text-2xl w-14 grid place-items-center aspect-square "
+			className="text-gray-100 bg-gray-300 rounded-lg font-semibold text-2xl w-14 grid place-items-center aspect-square "
 		>
 			{children}
 		</button>
