4e57ec734e628505e98a38141b235cb8c48ef9c9
Ido Kadosh <1234kadosh@gmail.com>
2024-07-30 14:12:44 +0300

    style: add z-index for Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,7 +8,7 @@ interface PropTypes {
 
 const Modal = ({ children, onCloseModal }: PropTypes) => {
 	return (
-		<div className="absolute inset-0 m-auto grid place-items-center shadow-[0_0_3em_0.2em_#555] bg-white max-w-fit max-h-fit text-clamp-lg py-[1em] px-[0.5em]">
+		<div className="absolute inset-0 m-auto grid place-items-center shadow-[0_0_3em_0.2em_#555] bg-white max-w-fit max-h-fit text-clamp-lg py-[1em] px-[0.5em] z-[9999]">
 			{children}
 			<button
 				className="border-[0.1em] text-[1.5em] rounded-full border-black absolute top-[0.5em] left-[0.5em]"
